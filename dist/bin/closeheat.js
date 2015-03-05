53b98d3e0ad9df503fef4ac4ab74958f812a8639
Domas <domas.bitvinskas@me.com>
2015-03-05 11:06:29 +0200

    Dont run updater everywhere - choose

--- dist/bin/closeheat.js
+++ dist/bin/closeheat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var Log, Updater, fs, path, pkg, program, _;
+var Log, Server, fs, path, pkg, program, _;
 
 program = require('commander');
 
@@ -12,119 +12,139 @@ path = require('path');
 
 pkg = require('../../package.json');
 
-Updater = require('../updater');
-
 Log = require('../log');
 
-new Updater().update().then(function() {
-  var Server;
-  program.version(pkg.version).usage('<keywords>');
-  program.command('create [app-name]').description('Creates a new app with clean setup and directory structure.').option('-f, --framework [name]', 'Framework').option('-t, --template [name]', 'Template').option('--javascript [name]', 'Javascript precompiler').option('--html [name]', 'HTML precompiler').option('--css [name]', 'CSS precompiler').option('--tmp [path]', 'The path of temporary directory when creating').option('--dist [path]', 'Path of destination of where to create app dir').option('--no-deploy', 'Do not create GitHub repo and closeheat app').action(function(name, opts) {
-    var Creator, includes_template_settings, settings, template_settings;
-    Creator = require('../creator');
-    settings = _.pick.apply(_, [opts, 'framework', 'template', 'javascript', 'html', 'css', 'dist', 'tmp', 'deploy']);
-    settings.name = name;
-    Log.logo();
-    template_settings = ['framework', 'template', 'javascript', 'html', 'css'];
-    includes_template_settings = _.any(_.keys(settings), function(setting) {
-      return _.contains(template_settings, setting);
-    });
-    if (includes_template_settings) {
-      return new Creator().createFromSettings(settings);
-    } else {
-      return new Creator().createFromPrompt(settings);
-    }
+program.version(pkg.version).usage('<keywords>');
+
+program.command('create [app-name]').description('Creates a new app with clean setup and directory structure.').option('-f, --framework [name]', 'Framework').option('-t, --template [name]', 'Template').option('--javascript [name]', 'Javascript precompiler').option('--html [name]', 'HTML precompiler').option('--css [name]', 'CSS precompiler').option('--tmp [path]', 'The path of temporary directory when creating').option('--dist [path]', 'Path of destination of where to create app dir').option('--no-deploy', 'Do not create GitHub repo and closeheat app').action(function(name, opts) {
+  var Creator, includes_template_settings, settings, template_settings;
+  Creator = require('../creator');
+  settings = _.pick.apply(_, [opts, 'framework', 'template', 'javascript', 'html', 'css', 'dist', 'tmp', 'deploy']);
+  settings.name = name;
+  Log.logo();
+  template_settings = ['framework', 'template', 'javascript', 'html', 'css'];
+  includes_template_settings = _.any(_.keys(settings), function(setting) {
+    return _.contains(template_settings, setting);
   });
-  program.command('server').description('Runs a server which builds and LiveReloads your app.').option('--ip [ip]', 'IP to run LiveReload on (default - localhost)').option('-p, --port [port]', 'Port to run server on (default - 4000)').action(function(opts) {
+  if (includes_template_settings) {
+    return new Creator().createFromSettings(settings);
+  } else {
+    return new Creator().createFromPrompt(settings);
+  }
+});
+
+program.command('server').description('Runs a server which builds and LiveReloads your app.').option('--ip [ip]', 'IP to run LiveReload on (default - localhost)').option('-p, --port [port]', 'Port to run server on (default - 4000)').action(function(opts) {
+  var Updater;
+  Updater = require('../updater');
+  return new Updater().update().then(function() {
     var Server;
     Server = require('../server');
     return new Server().start(opts);
   });
-  program.command('deploy').description('Deploys your app to closeheat.com via GitHub.').action(function() {
-    var Deployer;
-    Deployer = require('../deployer');
-    Log.logo();
-    return new Deployer().deploy();
-  });
-  program.command('log').description('Polls the log of the last deployment. Usable: git push origin master && closeheat log').action(function() {
-    var DeployLog;
-    DeployLog = require('../deploy_log');
-    Log.logo();
-    return new DeployLog().fromCurrentCommit();
-  });
-  program.command('open').description('Opens your deployed app in the browser.').action(function() {
-    var Deployer;
-    Deployer = require('../deployer');
-    return new Deployer().open();
-  });
-  program.command('apps').description('Shows a list of your deployed apps.').action(function() {
+});
+
+program.command('deploy').description('Deploys your app to closeheat.com via GitHub.').action(function() {
+  var Deployer;
+  Deployer = require('../deployer');
+  Log.logo();
+  return new Deployer().deploy();
+});
+
+program.command('log').description('Polls the log of the last deployment. Usable: git push origin master && closeheat log').action(function() {
+  var DeployLog;
+  DeployLog = require('../deploy_log');
+  Log.logo();
+  return new DeployLog().fromCurrentCommit();
+});
+
+program.command('open').description('Opens your deployed app in the browser.').action(function() {
+  var Deployer;
+  Deployer = require('../deployer');
+  return new Deployer().open();
+});
+
+program.command('apps').description('Shows a list of your deployed apps.').action(function() {
+  var Updater;
+  Updater = require('../updater');
+  return new Updater().update().then(function() {
     var Apps;
     Apps = require('../apps');
     return new Apps().list();
   });
-  program.command('login').option('-t, --token [access-token]', 'Access token from closeheat.com.').description('Log in to closeheat.com with this computer.').action(function(opts) {
-    var Authorizer;
-    Authorizer = require('../authorizer');
-    if (opts.token) {
-      return new Authorizer().saveToken(opts.token);
-    } else {
-      return new Authorizer().login();
-    }
+});
+
+program.command('login').option('-t, --token [access-token]', 'Access token from closeheat.com.').description('Log in to closeheat.com with this computer.').action(function(opts) {
+  var Authorizer;
+  Authorizer = require('../authorizer');
+  if (opts.token) {
+    return new Authorizer().saveToken(opts.token);
+  } else {
+    return new Authorizer().login();
+  }
+});
+
+program.command('clone [app-name]').description('Clones the closeheat app files.').action(function(app_name) {
+  var Apps, Cloner;
+  if (app_name) {
+    Cloner = require('../cloner');
+    return new Cloner().clone(app_name);
+  } else {
+    Apps = require('../apps');
+    return new Apps().list();
+  }
+});
+
+program.command('transform [type] [language]').description('Transforms files in current dir to other language (Experimental).').action(function(type, language) {
+  var Dirs, Transformer, dirs, settings;
+  Dirs = require('../dirs');
+  Transformer = require('../transformer');
+  Log.logo();
+  dirs = new Dirs({
+    name: 'transforming',
+    src: process.cwd(),
+    dist: process.cwd()
   });
-  program.command('clone [app-name]').description('Clones the closeheat app files.').action(function(app_name) {
-    var Apps, Cloner;
-    if (app_name) {
-      Cloner = require('../cloner');
-      return new Cloner().clone(app_name);
-    } else {
-      Apps = require('../apps');
-      return new Apps().list();
-    }
-  });
-  program.command('transform [type] [language]').description('Transforms files in current dir to other language (Experimental).').action(function(type, language) {
-    var Dirs, Transformer, dirs, settings;
-    Dirs = require('../dirs');
-    Transformer = require('../transformer');
-    Log.logo();
-    dirs = new Dirs({
-      name: 'transforming',
-      src: process.cwd(),
-      dist: process.cwd()
-    });
-    settings = {};
-    settings[type] = language;
-    return new Transformer(dirs).transform(settings).then((function(_this) {
-      return function() {
-        return console.log('transformed', settings);
-      };
-    })(this));
-  });
-  program.command('help').description('Displays this menu.').action(function() {
+  settings = {};
+  settings[type] = language;
+  return new Transformer(dirs).transform(settings).then((function(_this) {
+    return function() {
+      return console.log('transformed', settings);
+    };
+  })(this));
+});
+
+program.command('help').description('Displays this menu.').action(function() {
+  var Updater;
+  Updater = require('../updater');
+  return new Updater().update().then(function() {
     Log.logo(0);
     return program.help();
   });
-  program.command('postinstall').description('This is run after the install for easy instructions.').action(function() {
-    var Apps, Color;
-    Color = require('../color');
-    Log.br();
-    Log.p('Installation successful.');
-    Log.p('------------------------');
-    Log.p("Running " + (Color.violet('closeheat apps')) + " command.");
-    Log.br();
-    Apps = require('../apps');
-    return new Apps().list().then(function() {
-      Log.br();
-      return Log.p("Type " + (Color.violet('closeheat help')) + " for more awesomeness.");
-    });
-  });
-  program.parse(process.argv);
-  if (!program.args.length) {
-    if (fs.existsSync('index.html') || fs.existsSync('index.jade')) {
-      Server = require('../server');
-      return new Server().start();
-    } else {
-      Log.logo(0);
-      return program.help();
-    }
-  }
 });
+
+program.command('postinstall').description('This is run after the install for easy instructions.').action(function() {
+  var Apps, Color;
+  Color = require('../color');
+  Log.br();
+  Log.p('Installation successful.');
+  Log.p('------------------------');
+  Log.p("Running " + (Color.violet('closeheat apps')) + " command.");
+  Log.br();
+  Apps = require('../apps');
+  return new Apps().list().then(function() {
+    Log.br();
+    return Log.p("Type " + (Color.violet('closeheat help')) + " for more awesomeness.");
+  });
+});
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  if (fs.existsSync('index.html') || fs.existsSync('index.jade')) {
+    Server = require('../server');
+    new Server().start();
+  } else {
+    Log.logo(0);
+    program.help();
+  }
+}
--- src/bin/closeheat.coffee
+++ src/bin/closeheat.coffee
@@ -4,173 +4,177 @@ fs = require 'fs'
 path = require 'path'
 
 pkg = require '../../package.json'
-Updater = require '../updater'
 Log = require '../log'
 
-new Updater().update().then ->
-  program
-    .version(pkg.version)
-    .usage('<keywords>')
+program
+  .version(pkg.version)
+  .usage('<keywords>')
 
-  program
-    .command('create [app-name]')
-    .description('Creates a new app with clean setup and directory structure.')
-    .option('-f, --framework [name]', 'Framework')
-    .option('-t, --template [name]', 'Template')
-    .option('--javascript [name]', 'Javascript precompiler')
-    .option('--html [name]', 'HTML precompiler')
-    .option('--css [name]', 'CSS precompiler')
-    .option('--tmp [path]', 'The path of temporary directory when creating')
-    .option('--dist [path]', 'Path of destination of where to create app dir')
-    .option('--no-deploy', 'Do not create GitHub repo and closeheat app')
-    .action (name, opts) ->
-      Creator = require '../creator'
+program
+  .command('create [app-name]')
+  .description('Creates a new app with clean setup and directory structure.')
+  .option('-f, --framework [name]', 'Framework')
+  .option('-t, --template [name]', 'Template')
+  .option('--javascript [name]', 'Javascript precompiler')
+  .option('--html [name]', 'HTML precompiler')
+  .option('--css [name]', 'CSS precompiler')
+  .option('--tmp [path]', 'The path of temporary directory when creating')
+  .option('--dist [path]', 'Path of destination of where to create app dir')
+  .option('--no-deploy', 'Do not create GitHub repo and closeheat app')
+  .action (name, opts) ->
+    Creator = require '../creator'
 
-      settings = _.pick(
-        [
-          opts
-          'framework'
-          'template'
-          'javascript'
-          'html'
-          'css'
-          'dist'
-          'tmp'
-          'deploy'
-        ]...
-      )
-
-      settings.name = name
-
-      Log.logo()
-
-      template_settings = [
+    settings = _.pick(
+      [
+        opts
         'framework'
         'template'
         'javascript'
         'html'
         'css'
-      ]
+        'dist'
+        'tmp'
+        'deploy'
+      ]...
+    )
 
-      includes_template_settings = _.any _.keys(settings), (setting) ->
-        _.contains(template_settings, setting)
+    settings.name = name
 
-      if includes_template_settings
-        new Creator().createFromSettings(settings)
-      else
-        new Creator().createFromPrompt(settings)
+    Log.logo()
 
-  program
-    .command('server')
-    .description('Runs a server which builds and LiveReloads your app.')
-    .option('--ip [ip]', 'IP to run LiveReload on (default - localhost)')
-    .option('-p, --port [port]', 'Port to run server on (default - 4000)')
-    .action (opts) ->
+    template_settings = [
+      'framework'
+      'template'
+      'javascript'
+      'html'
+      'css'
+    ]
+
+    includes_template_settings = _.any _.keys(settings), (setting) ->
+      _.contains(template_settings, setting)
+
+    if includes_template_settings
+      new Creator().createFromSettings(settings)
+    else
+      new Creator().createFromPrompt(settings)
+
+program
+  .command('server')
+  .description('Runs a server which builds and LiveReloads your app.')
+  .option('--ip [ip]', 'IP to run LiveReload on (default - localhost)')
+  .option('-p, --port [port]', 'Port to run server on (default - 4000)')
+  .action (opts) ->
+    Updater = require '../updater'
+    new Updater().update().then ->
       Server = require '../server'
       new Server().start(opts)
 
-  program
-    .command('deploy')
-    .description('Deploys your app to closeheat.com via GitHub.')
-    .action ->
-      Deployer = require '../deployer'
+program
+  .command('deploy')
+  .description('Deploys your app to closeheat.com via GitHub.')
+  .action ->
+    Deployer = require '../deployer'
 
-      Log.logo()
-      new Deployer().deploy()
+    Log.logo()
+    new Deployer().deploy()
 
-  program
-    .command('log')
-    .description('Polls the log of the last deployment. Usable: git push origin master && closeheat log')
-    .action ->
-      DeployLog = require '../deploy_log'
+program
+  .command('log')
+  .description('Polls the log of the last deployment. Usable: git push origin master && closeheat log')
+  .action ->
+    DeployLog = require '../deploy_log'
 
-      Log.logo()
-      new DeployLog().fromCurrentCommit()
+    Log.logo()
+    new DeployLog().fromCurrentCommit()
 
-  program
-    .command('open')
-    .description('Opens your deployed app in the browser.')
-    .action ->
-      Deployer = require '../deployer'
+program
+  .command('open')
+  .description('Opens your deployed app in the browser.')
+  .action ->
+    Deployer = require '../deployer'
 
-      new Deployer().open()
+    new Deployer().open()
 
-  program
-    .command('apps')
-    .description('Shows a list of your deployed apps.')
-    .action ->
+program
+  .command('apps')
+  .description('Shows a list of your deployed apps.')
+  .action ->
+    Updater = require '../updater'
+    new Updater().update().then ->
       Apps = require '../apps'
 
       new Apps().list()
 
-  program
-    .command('login')
-    .option('-t, --token [access-token]', 'Access token from closeheat.com.')
-    .description('Log in to closeheat.com with this computer.')
-    .action (opts) ->
-      Authorizer = require '../authorizer'
+program
+  .command('login')
+  .option('-t, --token [access-token]', 'Access token from closeheat.com.')
+  .description('Log in to closeheat.com with this computer.')
+  .action (opts) ->
+    Authorizer = require '../authorizer'
 
-      if opts.token
-        new Authorizer().saveToken(opts.token)
-      else
-        new Authorizer().login()
-
-  program
-    .command('clone [app-name]')
-    .description('Clones the closeheat app files.')
-    .action (app_name) ->
-      if app_name
-        Cloner = require '../cloner'
-        new Cloner().clone(app_name)
-      else
-        Apps = require '../apps'
-        new Apps().list()
-
-  program
-    .command('transform [type] [language]')
-    .description('Transforms files in current dir to other language (Experimental).')
-    .action (type, language) ->
-      Dirs = require '../dirs'
-      Transformer = require '../transformer'
-
-      Log.logo()
-      dirs = new Dirs(name: 'transforming', src: process.cwd(), dist: process.cwd())
-
-      settings = {}
-      settings[type] = language
-
-      new Transformer(dirs).transform(settings).then =>
-        console.log('transformed', settings)
-
-  program
-    .command('help')
-    .description('Displays this menu.')
-    .action ->
-      Log.logo(0)
-      program.help()
-
-  program
-    .command('postinstall')
-    .description('This is run after the install for easy instructions.')
-    .action ->
-      Color = require '../color'
-      Log.br()
-      Log.p('Installation successful.')
-      Log.p('------------------------')
-      Log.p("Running #{Color.violet('closeheat apps')} command.")
-      Log.br()
-
-      Apps = require '../apps'
-      new Apps().list().then ->
-        Log.br()
-        Log.p("Type #{Color.violet('closeheat help')} for more awesomeness.")
-
-  program.parse(process.argv)
-
-  unless program.args.length
-    if fs.existsSync('index.html') || fs.existsSync('index.jade')
-      Server = require '../server'
-      new Server().start()
+    if opts.token
+      new Authorizer().saveToken(opts.token)
     else
+      new Authorizer().login()
+
+program
+  .command('clone [app-name]')
+  .description('Clones the closeheat app files.')
+  .action (app_name) ->
+    if app_name
+      Cloner = require '../cloner'
+      new Cloner().clone(app_name)
+    else
+      Apps = require '../apps'
+      new Apps().list()
+
+program
+  .command('transform [type] [language]')
+  .description('Transforms files in current dir to other language (Experimental).')
+  .action (type, language) ->
+    Dirs = require '../dirs'
+    Transformer = require '../transformer'
+
+    Log.logo()
+    dirs = new Dirs(name: 'transforming', src: process.cwd(), dist: process.cwd())
+
+    settings = {}
+    settings[type] = language
+
+    new Transformer(dirs).transform(settings).then =>
+      console.log('transformed', settings)
+
+program
+  .command('help')
+  .description('Displays this menu.')
+  .action ->
+    Updater = require '../updater'
+    new Updater().update().then ->
       Log.logo(0)
       program.help()
+
+program
+  .command('postinstall')
+  .description('This is run after the install for easy instructions.')
+  .action ->
+    Color = require '../color'
+    Log.br()
+    Log.p('Installation successful.')
+    Log.p('------------------------')
+    Log.p("Running #{Color.violet('closeheat apps')} command.")
+    Log.br()
+
+    Apps = require '../apps'
+    new Apps().list().then ->
+      Log.br()
+      Log.p("Type #{Color.violet('closeheat help')} for more awesomeness.")
+
+program.parse(process.argv)
+
+unless program.args.length
+  if fs.existsSync('index.html') || fs.existsSync('index.jade')
+    Server = require '../server'
+    new Server().start()
+  else
+    Log.logo(0)
+    program.help()
